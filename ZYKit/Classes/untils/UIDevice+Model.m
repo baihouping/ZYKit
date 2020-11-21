c295e2f2dc7f5a2b611acb0b26e87a333a4ef89c
白候平 <>
2020-11-21 21:20:33 +0800

    0.3.2

---
+++ ZYKit/Classes/untils/UIDevice+Model.m
@@ -0,0 +1,175 @@
+//
+//  UIDevice+Model.m
+//  winnowerLife
+//
+//  Created by 白候平 on 2019/7/3.
+//  Copyright © 2019 白候平. All rights reserved.
+//
+
+#import "UIDevice+Model.h"
+#import <sys/sysctl.h>
+#import <sys/sockio.h>
+#import <sys/socket.h>
+#import <arpa/inet.h>
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+#import  <sys/ioctl.h>
+
+@implementation UIDevice (Model)
+
+- (NSString*)ipAddresses {
+    int sockfd =socket(AF_INET,SOCK_DGRAM,0);
+    // if (sockfd < 0) return nil;
+    NSMutableArray*ips = [NSMutableArray array];
+    int BUFFERSIZE = 4096;
+    struct ifconf ifc;
+    char buffer[BUFFERSIZE], *ptr, lastname[IFNAMSIZ], *cptr;
+    struct ifreq *ifr, ifrcopy;
+    ifc.ifc_len = BUFFERSIZE;
+    ifc.ifc_buf = buffer;
+    if(ioctl(sockfd,SIOCGIFCONF, &ifc) >=0){
+        for(ptr = buffer; ptr < buffer + ifc.ifc_len; ){
+            ifr = (struct ifreq*)ptr;
+            int len =sizeof(struct sockaddr);
+            if(ifr->ifr_addr.sa_len> len) {
+                len = ifr->ifr_addr.sa_len;
+            }
+            ptr +=sizeof(ifr->ifr_name) + len;
+            if(ifr->ifr_addr.sa_family!=AF_INET)continue;
+            if((cptr = (char*)strchr(ifr->ifr_name,':')) !=NULL) *cptr =0;
+            if(strncmp(lastname, ifr->ifr_name,IFNAMSIZ) ==0)continue;
+            memcpy(lastname, ifr->ifr_name,IFNAMSIZ);
+            ifrcopy = *ifr;
+            ioctl(sockfd,SIOCGIFFLAGS, &ifrcopy);
+            if((ifrcopy.ifr_flags&IFF_UP) ==0)continue;
+            NSString *ip = [NSString stringWithFormat:@"%s",inet_ntoa(((struct sockaddr_in*)&ifr->ifr_addr)->sin_addr)];
+            [ips addObject:ip];
+        }
+    }
+    close(sockfd);
+    NSString *deviceIP =@"";
+    for(int i=0; i < ips.count; i++){
+        if(ips.count>0){
+            deviceIP = [NSString stringWithFormat:@"%@",ips.lastObject];
+        }
+    }
+    return deviceIP;
+}
+
+- (NSString *)machineModel {
+    static dispatch_once_t one;
+    static NSString *model;
+    dispatch_once(&one, ^{
+        size_t size;
+        sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+        char *machine = malloc(size);
+        sysctlbyname("hw.machine", machine, &size, NULL, 0);
+        model = [NSString stringWithUTF8String:machine];
+        free(machine);
+    });
+    return model;
+}
+
+- (NSString *)machineModelName {
+    static dispatch_once_t one;
+    static NSString *name;
+    dispatch_once(&one, ^{
+        NSString *model = [self machineModel];
+        if (!model) return;
+        NSDictionary *dic = @{
+                              @"Watch1,1" : @"Apple Watch 38mm",
+                              @"Watch1,2" : @"Apple Watch 42mm",
+                              @"Watch2,3" : @"Apple Watch Series 2 38mm",
+                              @"Watch2,4" : @"Apple Watch Series 2 42mm",
+                              @"Watch2,6" : @"Apple Watch Series 1 38mm",
+                              @"Watch1,7" : @"Apple Watch Series 1 42mm",
+                              
+                              @"iPod1,1" : @"iPod touch 1",
+                              @"iPod2,1" : @"iPod touch 2",
+                              @"iPod3,1" : @"iPod touch 3",
+                              @"iPod4,1" : @"iPod touch 4",
+                              @"iPod5,1" : @"iPod touch 5",
+                              @"iPod7,1" : @"iPod touch 6",
+                              
+                              @"iPhone1,1" : @"iPhone 1G",
+                              @"iPhone1,2" : @"iPhone 3G",
+                              @"iPhone2,1" : @"iPhone 3GS",
+                              @"iPhone3,1" : @"iPhone 4 (GSM)",
+                              @"iPhone3,2" : @"iPhone 4",
+                              @"iPhone3,3" : @"iPhone 4 (CDMA)",
+                              @"iPhone4,1" : @"iPhone 4S",
+                              @"iPhone5,1" : @"iPhone 5",
+                              @"iPhone5,2" : @"iPhone 5",
+                              @"iPhone5,3" : @"iPhone 5c",
+                              @"iPhone5,4" : @"iPhone 5c",
+                              @"iPhone6,1" : @"iPhone 5s",
+                              @"iPhone6,2" : @"iPhone 5s",
+                              @"iPhone7,1" : @"iPhone 6 Plus",
+                              @"iPhone7,2" : @"iPhone 6",
+                              @"iPhone8,1" : @"iPhone 6s",
+                              @"iPhone8,2" : @"iPhone 6s Plus",
+                              @"iPhone8,4" : @"iPhone SE",
+                              @"iPhone9,1" : @"iPhone 7",
+                              @"iPhone9,2" : @"iPhone 7 Plus",
+                              @"iPhone9,3" : @"iPhone 7",
+                              @"iPhone9,4" : @"iPhone 7 Plus",
+                              @"iPhone10,1" : @"Phone 8 Global",
+                              @"iPhone10,2" : @"iPhone 8 Plus Global",
+                              @"iPhone10,3" : @"iPhone X Global",
+                              @"iPhone10,4" : @"iPhone 8 GSM",
+                              @"iPhone10,5" : @"iiPhone 8 Plus GSM",
+                              @"iPhone10,6" : @"iPhone X GSM",
+                              @"iPhone11,2" : @"iPhone XS",
+                              @"iPhone11,4" : @"iPhone iPhone XS Max (China)",
+                              @"iPhone11,6" : @"iPhone XS Max",
+                              @"iPhone11,8" : @"iPhone XR",
+                              
+                              @"iPad1,1" : @"iPad 1",
+                              @"iPad2,1" : @"iPad 2 (WiFi)",
+                              @"iPad2,2" : @"iPad 2 (GSM)",
+                              @"iPad2,3" : @"iPad 2 (CDMA)",
+                              @"iPad2,4" : @"iPad 2",
+                              @"iPad2,5" : @"iPad mini 1",
+                              @"iPad2,6" : @"iPad mini 1",
+                              @"iPad2,7" : @"iPad mini 1",
+                              @"iPad3,1" : @"iPad 3 (WiFi)",
+                              @"iPad3,2" : @"iPad 3 (4G)",
+                              @"iPad3,3" : @"iPad 3 (4G)",
+                              @"iPad3,4" : @"iPad 4",
+                              @"iPad3,5" : @"iPad 4",
+                              @"iPad3,6" : @"iPad 4",
+                              @"iPad4,1" : @"iPad Air",
+                              @"iPad4,2" : @"iPad Air",
+                              @"iPad4,3" : @"iPad Air",
+                              @"iPad4,4" : @"iPad mini 2",
+                              @"iPad4,5" : @"iPad mini 2",
+                              @"iPad4,6" : @"iPad mini 2",
+                              @"iPad4,7" : @"iPad mini 3",
+                              @"iPad4,8" : @"iPad mini 3",
+                              @"iPad4,9" : @"iPad mini 3",
+                              @"iPad5,1" : @"iPad mini 4",
+                              @"iPad5,2" : @"iPad mini 4",
+                              @"iPad5,3" : @"iPad Air 2",
+                              @"iPad5,4" : @"iPad Air 2",
+                              @"iPad6,3" : @"iPad Pro (9.7 inch)",
+                              @"iPad6,4" : @"iPad Pro (9.7 inch)",
+                              @"iPad6,7" : @"iPad Pro (12.9 inch)",
+                              @"iPad6,8" : @"iPad Pro (12.9 inch)",
+                              
+                              @"AppleTV2,1" : @"Apple TV 2",
+                              @"AppleTV3,1" : @"Apple TV 3",
+                              @"AppleTV3,2" : @"Apple TV 3",
+                              @"AppleTV5,3" : @"Apple TV 4",
+                              
+                              @"i386" : @"Simulator x86",
+                              @"x86_64" : @"Simulator x64",
+                              };
+        name = dic[model];
+        if (!name) name = model;
+    });
+    return name;
+}
+
+@end
